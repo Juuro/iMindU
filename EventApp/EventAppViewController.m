019cdeb777ce1a5a7e4f0c341bf51bf1279ec08d
Juuro <mail@sebastian-engel.de>
2011-05-06 02:15:17 +0200

    Retina graphics. (Except backhround.)

--- EventApp/EventAppViewController.m
+++ EventApp/EventAppViewController.m
@@ -219,7 +219,7 @@
 //Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
 - (void)viewDidLoad
 {
-    [titleField becomeFirstResponder]; 
+    //[titleField becomeFirstResponder]; 
     [super viewDidLoad];
 }
 
